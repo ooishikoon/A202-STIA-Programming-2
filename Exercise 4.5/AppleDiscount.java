93fb14d245a66da6da295ea6d17bc43d4cc2019c
ooishikoon <80800379+ooishikoon@users.noreply.github.com>
2021-05-17 19:39:30 +0800

    Add files via upload

---
+++ Exercise 4.5/AppleDiscount.java
@@ -0,0 +1,44 @@
+package Exercise4_5;
+
+//abstract class implements interface
+//lebih kurang sama dengan inherit guna extends
+class AppleDiscount implements Discount{
+	
+	public float rateOfDiscount() {
+		return 0.15f;
+	}
+	
+} //end class AppleDiscount
+
+class RedAppleDiscount implements Discount{
+
+	public float rateOfDiscount() {
+		return 0.25f;
+	}
+	
+} //end class RedAppleDiscount
+
+class GreenAppleDiscount implements Discount{
+
+	public float rateOfDiscount() {
+		return 0.35f;
+	}
+	
+} //end class GreeenAppleDiscount
+
+class AvocadoDiscount implements Discount{
+
+	public float rateOfDiscount() {
+		return 0.12f;
+	}
+	
+} //end class DurianDiscount
+
+
+class BlueberryDiscount implements Discount{
+
+	public float rateOfDiscount() {
+		return 0.18f;
+	}
+	
+} //end class MangoDiscount
---
+++ Exercise 4.5/Discount.java
@@ -0,0 +1,7 @@
+package Exercise4_5;
+
+interface Discount { //this class call as interface bcoz it will function similar to super class
+	
+	  float rateOfDiscount(); //method with no body
+
+}
